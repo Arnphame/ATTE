e6fe834014efab4d1ad941541f9a9271d8444024
Arnas Damasickis <arnas.damasickis@ktu.edu>
2018-03-18 17:36:56 +0200

    Created LOGIN, LOGOUT, REGISTER forms, needs design badly...

---
+++ First_ND/register.php
@@ -0,0 +1,13 @@
+<html>
+<pre>
+<form method="get" action="signup.php">
+    <input id="login" name="login" type="text">
+    <label>User name: </label>
+    <input id="pass" name="pass" type="password">
+    <label>Password: </label>
+    <input id="pass2" name="pass2" type="password">
+    <label>Retype password: </label>
+    <input name="submit" type="submit" value="Sign Up">
+</form>
+</pre>
+</html>
