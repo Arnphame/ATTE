0d2ce9b63eb943134e4e1e3900cc7047e92652a2
Tadas Andriuska <tadas.andriuska@ktu.edu>
2018-03-20 15:13:16 +0200

    Adding error handling

--- First_ND/register.php
+++ First_ND/register.php
@@ -1,20 +1,24 @@
+<?php
+session_start();
+if(empty($_SESSION['error']))
+    $errMsg = "";
+else
+    $errMsg = $_SESSION['error'];
+?>
 <html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="Styles/styles.css">
-		<link href='https://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet'>
-	</head>
-	<header>Registration</header>
-	<div id="container_signup">
-		<form method="get" action="signup.php">
-			<label for="username">Username:</label>
-		    <input id="login" name="login" type="text">
-		    <label for="pass">Password:</label>
-		    <input id="pass" name="pass" type="password">
-		    <label for="pass2">Retype password:</label>
-		    <input id="pass2" name="pass2" type="password">
-		    <div id="lower">
-		    	<input name="submit" type="submit" value="Sign Up">
-			</div>
-		</form>
-	</div>
+<pre>
+<form method="post" action="signup.php">
+    <input id="login" name="login" type="text">
+    <label>User name: </label>
+    <input id="pass" name="pass" type="password">
+    <label>Password: </label>
+    <input id="pass2" name="pass2" type="password">
+    <label>Retype password: </label>
+    <input name="submit" type="submit" value="Sign Up"">
+   <!-- <span style="color: #FF0000">  <?php// echo $errMsg?></span>-->
+</form>
+</pre>
+</html>
+<html>
+<a href="Index.php">Back to login page </a>
 </html>
