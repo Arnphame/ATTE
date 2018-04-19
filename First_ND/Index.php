20498bace62abdc7f528b93bf0fb02a718d4b357
Arnas Damasickis <arnas.damasickis@ktu.edu>
2018-04-19 22:18:10 +0300

    Semester Project start

--- First_ND/Index.php
+++ First_ND/Index.php
@@ -5,12 +5,6 @@
  * Date: 18.3.18
  * Time: 14.46
  */
-
-<<<<<<< HEAD
-//include ('Login.php');
-
-=======
->>>>>>> 20f2d7963c7504fafd39241e5284d065e0058737
 if(isset($_SESSION['logged in']))
 {
     header("location: Profile.php");
@@ -32,19 +26,10 @@ if(isset($_SESSION['logged in']))
 			    <label for="pass">Password:</label>
 			    <input id="pass" name="pass" type="password">
 			    <div id="lower">
-<<<<<<< HEAD
-			    	<!--<input type="checkbox"><label for="checkbox">Keep me loged in</label>-->
-			    	<input name="submit" type="submit" value="Login">			    	
-			    </div>
-			</form>
-			<a class="reg" href="register.php">Register</a>
-			<!--<a class="reg" href="forgot.php" style="position: fixed; margin-left: 190px;">Forgot password?</a>-->
-=======
 			    	<input name="submit" type="submit" value="Login">
 			    </div>
 			</form>
 			<a class="reg" href="register.php">Register</a>
->>>>>>> 20f2d7963c7504fafd39241e5284d065e0058737
 		</div>
 	</body>
 </html>
--- First_ND/signup.php
+++ First_ND/signup.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-=======
->>>>>>> 20f2d7963c7504fafd39241e5284d065e0058737
 $con = mysqli_connect("localhost", "arn", "123", "users");
 
 $login = $_GET['login'];
@@ -11,11 +7,6 @@ $pass2 = $_GET['pass2'];
 
 $check = mysqli_query($con, "SELECT login FROM user WHERE login='$login'");
 $rows = mysqli_num_rows($check);
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 20f2d7963c7504fafd39241e5284d065e0058737
 if ($rows == 0)
 {
     if ($pass != $pass2)
@@ -26,34 +17,18 @@ if ($rows == 0)
         $insert = mysqli_query($con, "INSERT INTO user (login, password) VALUES ('$login','$hashed_password')");
         ?> 
         <html> 
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <link rel="stylesheet" type="text/css" href="/styles.css">
-=======
             <link rel="stylesheet" type="text/css" href="Styles/styles.css">
->>>>>>> 20f2d7963c7504fafd39241e5284d065e0058737
-=======
-            <link rel="stylesheet" type="text/css" href="Styles/styles.css">
->>>>>>> 13ca003cbf40e4770e4b78bb3b9deb431d5b3a1d
             <link href='https://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet'>
             <header>Your registration is complete.</header> 
         </html> <?php
     }
-<<<<<<< HEAD
-} 
-=======
 }
->>>>>>> 20f2d7963c7504fafd39241e5284d065e0058737
 else
 {
     header("Location:register.php");
 }
 ?>
 <html>
-<<<<<<< HEAD
-<<<<<<< 13613a135fce884aa39d0bfd3af71a057a8bd216
-=======
->>>>>>> 20f2d7963c7504fafd39241e5284d065e0058737
     <head>
         <link rel="stylesheet" type="text/css" href="Styles/styles.css">
         <link href='https://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet'>
