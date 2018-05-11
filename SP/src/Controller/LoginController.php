4756a550cc7088e79f7bbcd389fc75d0d19ecff8
Tadas Andriuska <tadas.andriuska@ktu.edu>
2018-05-11 16:22:11 +0300

    Added Auto DB related to User

--- SP/src/Controller/LoginController.php
+++ SP/src/Controller/LoginController.php
@@ -23,6 +23,8 @@ class LoginController extends Controller
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
+
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
