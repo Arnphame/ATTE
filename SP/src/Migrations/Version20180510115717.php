9e58af457fe9ae92970288263e7937a648f3d0e6
Arnas Damasickis <arnas.damasickis@ktu.edu>
2018-05-28 20:24:08 +0300

    .

---
+++ SP/src/Migrations/Version20180510115717.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180510115717 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE car (id INT AUTO_INCREMENT NOT NULL, make VARCHAR(191) NOT NULL, model VARCHAR(191) NOT NULL, year DATE NOT NULL, fuel_type VARCHAR(191) NOT NULL, UNIQUE INDEX UNIQ_773DE69D1ACC766E (make), UNIQUE INDEX UNIQ_773DE69DD79572D9 (model), UNIQUE INDEX UNIQ_773DE69DBB827337 (year), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE car');
+    }
+}
